e616921d870996d5f47728ae2ab6c29244c7202c
MADMAXIMUUS <maxim00nechaev@gmail.com>
2021-10-12 13:37:52 +0300

    Обновлена иконка и добавлен новый элемент для тестов

---
+++ UMLRedactor/Elements/ClassElement.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UMLRedactor.Elements
+{
+    public partial class ClassElement : UserControl
+    {
+        public ClassElement()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainGrid.RowDefinitions.Add(new RowDefinition());
+            TextBox tb = new TextBox()
+            {
+                VerticalContentAlignment = VerticalAlignment.Center,
+                FontSize = 16,
+                Height = 30,
+                BorderThickness = new Thickness(0, 0, 0, 2),
+                BorderBrush = Brushes.Black
+            };
+            Grid.SetRow(tb, MainGrid.RowDefinitions.Count - 2);
+            Grid.SetRow(AddButton, MainGrid.RowDefinitions.Count - 1);
+            MainGrid.Children.Add(tb);
+        }
+    }
+}
