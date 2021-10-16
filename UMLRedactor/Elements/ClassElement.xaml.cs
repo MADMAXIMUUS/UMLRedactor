430888d2eeeff4af3def80c1ca512406596e0a29
MADMAXIMUUS <maxim00nechaev@gmail.com>
2021-10-16 23:54:20 +0300

    Обновление трансформации объекта

--- UMLRedactor/Elements/ClassElement.xaml.cs
+++ UMLRedactor/Elements/ClassElement.xaml.cs
@@ -8,11 +8,16 @@ namespace UMLRedactor.Elements
 {
     public partial class ClassElement : UserControl
     {
-        private Point _anchorPoint, _currentPoint;
-        private bool _isInDrag;
-        private TranslateTransform _translateTransform = new TranslateTransform();
-
         private readonly List<TextBox> _tbs = new List<TextBox>();
+        private int _edgeType;
+
+        enum EdgeTypes
+        {
+            TopLeft = 1,
+            TopRight = 2,
+            BottomLeft = 3,
+            BottomRight = 4
+        }
 
         public ClassElement()
         {
@@ -37,47 +42,112 @@ namespace UMLRedactor.Elements
             MainGrid.Children.Add(tb);
             _tbs.Add(tb);
         }
-
-        private void ClassElement_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        
+        private void Mt_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            var element = sender as FrameworkElement;
-            _anchorPoint = e.GetPosition(null);
-            if (element != null) element.CaptureMouse();
-            _isInDrag = true;
-            e.Handled = true;
-        }
-
-        private void ClassElement_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (_isInDrag)
+            Point offset = e.GetPosition(Mt);
+            double offsetX = offset.X;
+            double offsetY = offset.Y;
+            if (Parent is Canvas canvasMain && canvasMain.Parent is MainWindow main)
             {
-                _currentPoint = e.GetPosition(null);
-
-                _translateTransform.X += _currentPoint.X - _anchorPoint.X;
-                _translateTransform.Y += (_currentPoint.Y - _anchorPoint.Y);
-                if (_currentPoint.X < Application.Current.MainWindow.RenderSize.Width &&
-                    _currentPoint.Y < Application.Current.MainWindow.RenderSize.Height
-                    && _currentPoint.X > 0 && _currentPoint.Y > 0)
-                {
-                    RenderTransform = _translateTransform;
-                    _anchorPoint = _currentPoint;
-                }
-                else
-                {
-                    _translateTransform = new TranslateTransform();
-                    RenderTransform = _translateTransform;
-                }
+                main.IsSizing = true;
+                main.SizingEdgeType = 0;
+                main.SizingPanel = this;
+                main.SizingEdge = sender as Border;
+                main.SizingOffsetX = offsetX;
+                main.SizingOffsetY = offsetY;
             }
         }
 
-        private void ClassElement_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void ClassElement_OnGotFocus(object sender, RoutedEventArgs e)
         {
-            if (_isInDrag)
+            Lt.Visibility = Visibility.Visible;
+            Rt.Visibility = Visibility.Visible;
+            Lb.Visibility = Visibility.Visible;
+            Rb.Visibility = Visibility.Visible;
+            Mt.Visibility = Visibility.Visible;
+            Mb.Visibility = Visibility.Visible;
+        }
+
+        private void ClassElement_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            Lt.Visibility = Visibility.Hidden;
+            Rt.Visibility = Visibility.Hidden;
+            Lb.Visibility = Visibility.Hidden;
+            Rb.Visibility = Visibility.Hidden;
+            Mt.Visibility = Visibility.Hidden;
+            Mb.Visibility = Visibility.Hidden;
+        }
+
+        private void Lt_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.SizeNWSE;
+        }
+
+        private void Rt_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.SizeNESW;
+        }
+
+        private void Lb_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.SizeNESW;
+        }
+
+        private void Rb_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.SizeNWSE;
+        }
+
+        private void Mt_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+
+        private void Lt_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _edgeType = (int)EdgeTypes.TopLeft;
+            set_Sizing(sender, e);
+        }
+
+        private void Rt_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _edgeType = (int)EdgeTypes.TopRight;
+            set_Sizing(sender, e);
+        }
+
+        private void Lb_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _edgeType = (int)EdgeTypes.BottomLeft;
+            set_Sizing(sender, e);
+        }
+
+        private void Rb_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _edgeType = (int)EdgeTypes.BottomRight;
+            set_Sizing(sender, e);
+        }
+
+        private void set_Sizing(object sender, MouseButtonEventArgs e)
+        {
+            Canvas canvasMain = Parent as Canvas;
+            if (canvasMain?.Parent is MainWindow main)
             {
-                if (sender is FrameworkElement element) element.ReleaseMouseCapture();
-                _isInDrag = false;
-                e.Handled = true;
+                main.IsSizing = true;
+                main.SizingEdgeType = _edgeType;
+                main.SizingPanel = this;
+                main.SizingEdge = sender as Border;
             }
+        }
+
+        private void Mt_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
         }
     }
 }
--- UMLRedactor/MainWindow.xaml.cs
+++ UMLRedactor/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UMLRedactor.Elements;
 
 namespace UMLRedactor
 {
@@ -7,14 +11,105 @@ namespace UMLRedactor
     /// </summary>
     public partial class MainWindow
     {
+        public bool IsSizing;
+        public int SizingEdgeType;
+        public double SizingOffsetX;
+        public double SizingOffsetY;
+        public UserControl SizingPanel;
+
+        public Border SizingEdge;
+
+        enum EdgeTypes
+        {
+            TopMove = 0,
+            TopLeft = 1,
+            TopRight = 2,
+            BottomLeft = 3,
+            BottomRight = 4
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) { }
+
+        private void set_Sizing(object sender, MouseEventArgs e)
         {
-            
+            if (IsSizing == false) return;
+            if (SizingEdgeType < 0) return;
+            if (SizingPanel == null) return;
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                IsSizing = false;
+            }
+            else
+            {
+                Point mousePoint = e.GetPosition(this);
+                double mouseX = mousePoint.X;
+                double mouseY = mousePoint.Y;
+                Point position = TranslatePoint(new Point(0, 0), SizingPanel);
+                double posX = -position.X;
+                double posY = -position.Y;
+                double diffX = (posX - mouseX);
+                double diffY = (posY - mouseY);
+                if (SizingEdgeType == (int)EdgeTypes.TopMove)
+                {
+                    double newLeft = mouseX - SizingOffsetX;
+                    double newTop = mouseY - SizingOffsetY;
+                    if (newLeft > 0) Canvas.SetLeft(SizingPanel, newLeft);
+                    if (newTop > 0) Canvas.SetTop(SizingPanel, newTop);
+                }
+                else
+                {
+                    if ((SizingEdgeType == (int)EdgeTypes.TopLeft) || (SizingEdgeType == (int)EdgeTypes.BottomLeft))
+                    {
+                        double new_Left = mouseX;
+                        double newWidth = (SizingPanel.ActualWidth) + diffX;
+                        if (new_Left > 0) Canvas.SetLeft(SizingPanel, new_Left);
+                        if (newWidth > 0) SizingPanel.Width = newWidth;
+                    }
+
+                    if ((SizingEdgeType == (int)EdgeTypes.TopLeft) || (SizingEdgeType == (int)EdgeTypes.TopRight))
+                    {
+                        double new_Top = mouseY;
+                        double newHeight = (SizingPanel.ActualHeight) + diffY;
+                        if (new_Top > 0) Canvas.SetTop(SizingPanel, new_Top);
+                        if (newHeight > 0) SizingPanel.Height = newHeight;
+                    }
+
+                    if ((SizingEdgeType == (int)EdgeTypes.TopRight) || (SizingEdgeType == (int)EdgeTypes.BottomRight))
+                    {
+                        double newWidth = mouseX - posX;
+                        if (newWidth > 0) SizingPanel.Width = newWidth;
+                    }
+
+                    if ((SizingEdgeType == (int)EdgeTypes.BottomLeft) || (SizingEdgeType == (int)EdgeTypes.BottomRight))
+                    {
+                        double newHeight = mouseY - posY;
+                        if (newHeight > 0) SizingPanel.Height = newHeight;
+                    }
+                }
+            }
+        }
+
+        private void DrawCanvas_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsSizing) set_Sizing(sender, e);
+        }
+
+        private void DrawCanvas_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsSizing)
+            {
+                IsSizing = false;
+                SizingEdgeType = -1;
+                SizingOffsetX = 0;
+                SizingOffsetY = 0;
+                SizingPanel = null;
+                SizingEdge = null;
+            }
         }
     }
 }
